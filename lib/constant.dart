232f568b92437d9a8a78f16beb4bb0cbf1c6de80
mohitvats2102 <mohitvatsktl@gmail.com>
2021-03-10 13:53:13 +0530

    Started Working On EP

---
+++ lib/constant.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+const Color kdarkTeal = Color(0xFF033249);
+
+const TextStyle kloginText = TextStyle(
+  color: Color(0xFFFF8038),
+  fontSize: 50,
+  fontWeight: FontWeight.w400,
+);
+
+const BoxDecoration kloginContainerDecoration = BoxDecoration(
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black12,
+      offset: Offset(10, -6),
+      spreadRadius: 2,
+      blurRadius: 20,
+    ),
+    BoxShadow(
+      color: Colors.black12,
+      offset: Offset(-20, -6),
+      spreadRadius: 0,
+      blurRadius: 40,
+    )
+  ],
+  color: Colors.white,
+  borderRadius: BorderRadius.only(
+    topLeft: Radius.circular(40),
+  ),
+);
+
+const InputDecoration klogininput = InputDecoration(
+  prefixIcon: Icon(
+    Icons.email_outlined,
+    color: kdarkTeal,
+  ),
+  //hintText: 'E-mail address',
+  labelText: 'E-mail',
+  border:
+      OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'screens/login_screen.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
@@ -14,15 +16,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Event Planner',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Event Planner'),
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
-          child: Text('Welcome To our New Project'),
-        ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        accentColor: Color(0xFFFF8038),
+        primaryColor: Color(0xFF033249),
+        fontFamily: 'Montserrat',
       ),
+      home: LoginScreen(),
+      // Scaffold(
+      //   appBar: AppBar(
+      //     title: Text('Event Planner'),
+      //     backgroundColor: Color(0xFF033249),
+      //   ),
+      //   body: Center(
+      //     child: Text('Welcome To our New Project'),
+      //   ),
+      // ),
     );
   }
 }
